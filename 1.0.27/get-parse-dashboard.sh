a3f6337765147dbe05358918d9b76f59cb461756
indrajit <eendroroy@gmail.com>
2017-06-09 23:31:56 +0600

    build version 1.0.27

---
+++ 1.0.27/get-parse-dashboard.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+\apt-get update
+\apt-get install unzip
+\rm -rf /var/cache/apt/
+
+\cd /tmp
+
+\wget https://github.com/parse-community/parse-dashboard/archive/1.0.27.zip
+\unzip 1.0.27.zip
+\mkdir -p /src
+\mv parse-dashboard-1.0.27/* /src/
+
+\cd /src
+
+\rm -rf .git
+\rm -rf .github
+\rm -rf .dockerignore
+\rm -rf .eslintrc.json
+\rm -rf .gitignore
+\rm -rf .travis.yml
+\rm -rf CHANGELOG.md
+\rm -rf CONTRIBUTING.md
+\rm -rf Dockerfile
+\rm -rf LICENSE
+\rm -rf README.md
